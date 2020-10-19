5e94093b3441009cf2504d108521fa045ef7f3e7
Luca Casonato <lucacasonato@yahoo.com>
2020-10-19 18:23:09 +0200

    initial commit

---
+++ mod.ts
@@ -0,0 +1,56 @@
+import { lookup } from "https://deno.land/x/media_types@v2.5.1/mod.ts";
+
+const originalfetch = window.fetch;
+
+async function fetch(
+  input: string | Request | URL,
+  init?: RequestInit,
+): Promise<Response> {
+  const url = typeof input === "string"
+    ? new URL(input)
+    : input instanceof Request
+    ? new URL(input.url)
+    : input;
+
+  if (url.protocol === "file:") {
+    // Only allow GET requests
+    if (init && init.method && init.method !== "GET") {
+      throw new TypeError(
+        `${init.method} is not a supported method for file:// URLs.`,
+      );
+    }
+
+    // Open the file, and convert to ReadableStream
+    const file = await Deno.open(url, { read: true });
+    const body = new ReadableStream<Uint8Array>({
+      start: async (controller) => {
+        for await (const chunk of Deno.iter(file)) {
+          controller.enqueue(chunk);
+        }
+        file.close();
+        controller.close();
+      },
+      cancel() {
+        file.close();
+      },
+    });
+
+    // Get meta information
+    const headers = new Headers();
+    const contentType = lookup(url.pathname);
+    if (contentType) {
+      headers.set("content-type", contentType);
+    }
+    const info = await Deno.stat(url);
+    if (info.mtime) {
+      headers.set("last-modified", info.mtime.toUTCString());
+    }
+
+    // Create 200 streaming response
+    return new Response(body, { status: 200, headers });
+  }
+
+  return originalfetch(input, init);
+}
+
+window.fetch = fetch;
---
+++ test.ts
@@ -0,0 +1,10 @@
+import "./mod.ts";
+import { assertEquals } from "https://deno.land/std@0.74.0/testing/asserts.ts";
+
+Deno.test("fetch local file URL", async () => {
+  const req = await fetch(new URL("./fixtures/test.json", import.meta.url));
+  assertEquals(req.status, 200);
+  assertEquals(req.headers.get("content-type"), "application/json");
+  const json = await req.json();
+  assertEquals(json, { hello: "world" });
+});
