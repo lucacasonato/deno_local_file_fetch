7c326942ef1778ce934eb8e510a2370de1040e55
Luca Casonato <lucacasonato@yahoo.com>
2021-04-08 13:43:59 +0200

    chore: update deps (#6)

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
 import "./polyfill.ts";
-import { assertEquals } from "https://deno.land/std@0.74.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.92.0/testing/asserts.ts";
 
 Deno.test("fetch local file URL", async () => {
   const req = await fetch(new URL("./fixtures/test.json", import.meta.url));
